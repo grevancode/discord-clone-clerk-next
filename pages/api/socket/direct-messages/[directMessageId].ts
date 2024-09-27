774e6b2eff10075257d05a1b6988df29468c8e12
grevancode <grevanrevenant@gmail.com>
2024-09-27 11:43:01 -0300

    tu mensaje aquí43

--- pages/api/socket/direct-messages/[directMessageId].ts
+++ pages/api/socket/direct-messages/[directMessageId].ts
@@ -9,6 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponseServerIo
 ) {
+  //hola
   if (req.method !== "DELETE" && req.method !== "PATCH")
     return res.status(405).json({ error: "Method not allowed" });
 
